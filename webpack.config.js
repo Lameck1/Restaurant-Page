eca12bab84f9284691e9590d6aa6a7baf1ea27b0
Lameck1 <lameck72@gmail.com>
2021-05-19 16:12:30 +0300

    Refactor HtmlWebpackPlugin template path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   plugins: [
-    new HtmlWebpackPlugin({ template: 'index.html' }),
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
   ],
   output: {
     filename: 'index.js',

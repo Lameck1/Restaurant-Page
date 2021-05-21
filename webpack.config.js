9a0e7cc85b58481165354ce7a241802e7dc510c2
Lameck1 <lameck72@gmail.com>
2021-05-21 23:19:37 +0300

    Set up deployment

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: 'Restaurant-Page',
     clean: true,
   },
   devtool: 'source-map',

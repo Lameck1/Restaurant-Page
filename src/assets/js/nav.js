50e027a3f6247d17db9a241d1955b86d155cf60e
Lameck1 <lameck72@gmail.com>
2021-05-21 21:20:32 +0300

    Add nav section
    Wrote code to include navigation bar to the page

---
+++ src/assets/js/nav.js
@@ -0,0 +1,14 @@
+import createElem from './createElem';
+
+export default () => {
+  // renders nav section
+  const content = document.getElementById('content');
+
+  const menu = createElem('MENU', { class: 'menu' });
+  const homePage = createElem('MENUITEM', { class: 'home-page' }, 'HOME');
+  const foodMenu = createElem('MENUITEM', { class: 'food-menu' }, 'MENU');
+  const contactPage = createElem('MENUITEM', { class: 'contact-page' }, 'CONTACT');
+  menu.append(homePage, foodMenu, contactPage);
+
+  document.body.insertBefore(menu, content);
+};

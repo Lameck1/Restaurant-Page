ad7c586f1ce6838277913bbcc5378ce216f585e8
Lameck1 <lameck72@gmail.com>
2021-05-21 21:30:49 +0300

    Code app logic
    Implemented app logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './assets/css/style.css';
+
+import renderNav from './assets/js/nav';
+import renderHomePage from './assets/js/home';
+import renderFoodMenu from './assets/js/menu';
+import renderContact from './assets/js/contact';
+
+renderNav();
+renderHomePage();
+
+const homePage = document.querySelector('.home-page');
+const foodMenu = document.querySelector('.food-menu');
+const contactPage = document.querySelector('.contact-page');
+const content = document.getElementById('content');
+
+homePage.addEventListener('click', () => {
+  content.innerHTML = '';
+  renderHomePage();
+});
+foodMenu.addEventListener('click', () => {
+  content.innerHTML = '';
+  renderFoodMenu();
+});
+
+contactPage.addEventListener('click', () => {
+  content.innerHTML = '';
+  renderContact();
+});

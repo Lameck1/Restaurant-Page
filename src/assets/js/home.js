77425c763ecc35aeeb2331334fcb2546ae60925c
Lameck1 <lameck72@gmail.com>
2021-05-21 21:24:35 +0300

    Add home section
    Added code to render the home page

---
+++ src/assets/js/home.js
@@ -0,0 +1,11 @@
+import createElem from './createElem';
+
+export default () => {
+  // renders home page
+  const content = document.getElementById('content');
+  const header = createElem('h1', {}, 'Pendo Dishes');
+  const phrase = createElem('p', { class: 'phrase' }, 'Hasty and tasty!');
+  const time = createElem('p', { class: 'time' }, 'Opening Hours - 24/7');
+  const about = createElem('p', { class: 'about' }, 'We are all about good food. But, besides that, we also offer top notch customer service. If you feel like this is not the best day for you to visit us but still need that good food, please go to our contact page and leave us a message or call us. You are always welcome.');
+  content.append(header, phrase, time, about);
+};
